774cdcf03866be7ae829c50546610dcd1426230c
khuunsh68 <goncalofarias10@ua.pt>
2023-11-21 11:17:14 +0000

    Erro de exceção corrigido no main

--- PimPamPum/src/main/java/org/projeto/Main.java
+++ PimPamPum/src/main/java/org/projeto/Main.java
@@ -4,8 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         PimPamPum programa = new PimPamPum();
-        int num = -37;
-        String resultado = programa.run(num);
-        System.out.println(num + " => " + resultado);
+        int num = 15;
+
+        try {
+            String resultado = programa.run(num);
+            System.out.println(num + " => " + resultado);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Exceção: " + e.getMessage());
+        }
     }
 }
