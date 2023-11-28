e2eee184f08ddcf466bfba158b65eb32a3de8b7b
khuunsh68 <goncalofarias10@ua.pt>
2023-11-28 09:37:30 +0000

    Versão 3.0

--- PimPamPum/src/main/java/org/projeto/PimPamPum.java
+++ PimPamPum/src/main/java/org/projeto/PimPamPum.java
@@ -3,48 +3,58 @@ public class PimPamPum {
     public String run(int num) {
 
         if (num < 0) {
-            throw new IllegalArgumentException("Numero escolhido nao e permitido!");
+            throw new IllegalArgumentException("Número escolhido não é permitido!");
         }
 
         StringBuilder resultado = new StringBuilder();
-        boolean alterado = false;
+        boolean[] alterado = {false};
 
+        verificaDivisibilidade(num, resultado, alterado);
+        verificaDigitosContidos(num, resultado, alterado);
+        imprimeProprioNumero(num, resultado, alterado);
+
+        return resultado.toString();
+    }
+
+    private void verificaDivisibilidade(int num, StringBuilder resultado, boolean[] alterado) {
         // Verificação se é divisível
         if (num % 3 == 0) {
             resultado.append("Pim");
-            alterado = true;
+            alterado[0] = true;
         }
         if (num % 5 == 0) {
             resultado.append("Pam");
-            alterado = true;
+            alterado[0] = true;
         }
         if (num % 7 == 0) {
             resultado.append("Pum");
-            alterado = true;
+            alterado[0] = true;
         }
+    }
 
+    private void verificaDigitosContidos(int num, StringBuilder resultado, boolean[] alterado) {
         // Verificação se contém 3, 5 ou 7
         String numStr = Integer.toString(num);
         for (char digito : numStr.toCharArray()) {
             if(digito == '3') {
                 resultado.append("Pim");
-                alterado = true;
+                alterado[0] = true;
             }
             if(digito == '5') {
                 resultado.append("Pam");
-                alterado = true;
+                alterado[0] = true;
             }
             if(digito == '7') {
                 resultado.append("Pum");
-                alterado = true;
+                alterado[0] = true;
             }
         }
+    }
 
+    private void imprimeProprioNumero(int num, StringBuilder resultado, boolean[] alterado) {
         // Não é divisível nem contém digitos iguais
-        if(!alterado) {
+        if(!alterado[0]) {
             resultado.append(num);
         }
-
-        return resultado.toString();
     }
 }
